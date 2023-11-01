39cc225593dd772abd0c7429b7c7025f51b0b5f9
Mithilesh Ditou Pandit <mithilesh450@gmail.com>
2023-11-01 18:32:41 +0530

    for deployement ignored pm2 file

--- node-backend/src/config/cors.js
+++ node-backend/src/config/cors.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://yourfrontenddomain.com'],
+  origin: ['http://localhost:3000', 'http://51.20.133.31'],
 }
 
 module.exports = corsOptions
